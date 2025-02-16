a34a8a1eaf94ecedadb41711606cbe20d23a94b0
Rimuru Tempest <dangghorahi944@gmail.com>
2025-02-16 22:06:23 +0545

    Removed venv and __pycache__

--- app/static/message.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    {% for category, message in messages %}
-        let msg = {{ message|tojson }}; // Proper escaping
-        alert(msg); // Or use a modal instead of alert
-    {% endfor %}
-};
