64ab44efb8ec5d15e1682a3c7f4e3afe82234e14
Rimuru41 <danggghorahi944@gmail.com>
2025-02-25 00:50:39 +0545

    Removed all ignored files from Git tracking

--- run.py
+++
@@ -1,9 +0,0 @@
-# run.py
-from app import create_app  
-from app.config import DevelopmentConfig, ProductionConfig,TestingConfig
-import os
-
-app = create_app(ProductionConfig)  
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Default to 5000 for local, but Render provides a PORT env
-    app.run(host="0.0.0.0", port=port)
